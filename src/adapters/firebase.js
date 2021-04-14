346e3855163b200f9f424157240173d49a8d6be3
Piero Lenzo <pierolenzo@gmail.com>
2021-04-15 00:28:50 +0200

    clean key

--- src/adapters/firebase.js
+++ src/adapters/firebase.js
@@ -1,12 +1,12 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA2Q9tVPmrUbuscXab9MYHyK9Of90PpzHI",
+  apiKey: "",
   authDomain: "twitter-clone-84818.firebaseapp.com",
   projectId: "twitter-clone-84818",
   storageBucket: "twitter-clone-84818.appspot.com",
-  messagingSenderId: "777291488225",
-  appId: "1:777291488225:web:fa7f172d3ff6df69e050a0"
+  messagingSenderId: "",
+  appId: ""
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
